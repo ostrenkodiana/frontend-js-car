d3159b9d558dd18888bc151d3086570ae0bc6f80
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-07-20 15:23:58 +0300

    done with all testes

---
+++ src/js/car.js
@@ -0,0 +1,41 @@
+function addPassengers() {
+	if (this.passengers.length < this.seats) {
+		this.passengers.push('true');
+	}
+}
+
+function removePassengers() {
+	if (this.passengers.length >= 1) {
+		this.passengers.pop();
+	}
+	if (this.passengers < 1) {
+		this.passengers.pop();
+		this.speed = 0;
+	}
+}
+
+function getNewSpeed(newSpeed) {
+	if (this.passengers[0] === undefined) {
+		return;
+	}
+	this.speed = this.defaultSpeed;
+	if (newSpeed >= this.maxSpeed) {
+		this.speed = this.maxSpeed;
+	}
+	if (newSpeed < this.maxSpeed) {
+		this.speed = newSpeed;
+	}
+}
+
+export default {
+	color: 'white',
+	dours: 4,
+	speed: 0,
+	defaultSpeed: 60,
+	maxSpeed: 100,
+	seats: 4,
+	passengers: [],
+	put: addPassengers,
+	land: removePassengers,
+	drive: getNewSpeed,
+};
